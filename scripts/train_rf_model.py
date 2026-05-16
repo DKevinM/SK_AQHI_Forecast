f82b49021bec939d5a87f24188f10959cbbd40d5
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-05-16 09:41:56 -0600

    Update train_rf_model.py

--- scripts/train_rf_model.py
+++ scripts/train_rf_model.py
@@ -6,6 +6,11 @@ import joblib
 import os
 os.makedirs("models", exist_ok=True)
 
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score
+)
 
 # ---------------------------
 # Load dataset
@@ -17,24 +22,44 @@ print("Columns:", data.columns)
 # ---------------------------
 # Targets (1 hour forecast)
 # ---------------------------
-data["PM25_target"] = data.groupby("station")["PM25"].shift(-1)
-data["O3_target"] = data.groupby("station")["O3"].shift(-1)
-data["NO2_target"] = data.groupby("station")["NO2"].shift(-1)
 
-data = data.dropna()
 
 
 # ---------------------------
 # Feature columns
 # ---------------------------
 feature_cols = [
-    "PM25_lag1","PM25_lag2","PM25_lag3","PM25_lag6","PM25_lag12",
-    "O3_lag1","O3_lag2","O3_lag3","O3_lag6","O3_lag12",
-    "NO2_lag1","NO2_lag2","NO2_lag3","NO2_lag6","NO2_lag12",
-    "WS","U","V","TEMP","RH",
-    "sin_hour","cos_hour",
-    "sin_doy","cos_doy",
-    "lat_norm","lon_norm","dist_center"
+
+    "AQHI",
+    "AQHI_lag1",
+    "AQHI_lag2",
+    "AQHI_lag3",
+    "AQHI_lag6",
+    "AQHI_lag12",
+    "AQHI_lag24",
+
+    "AQHI_change_1h",
+    "AQHI_change_3h",
+
+    "PM25",
+    "O3",
+    "NO2",
+
+    "WS",
+    "U",
+    "V",
+    "TEMP",
+    "RH",
+
+    "sin_hour",
+    "cos_hour",
+
+    "sin_doy",
+    "cos_doy",
+
+    "lat_norm",
+    "lon_norm",
+    "dist_center"
 ]
 
 X = data[feature_cols]
@@ -47,22 +72,35 @@ def train_model(target, name):
 
     y = data[target]
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, shuffle=False
-    )
+    split_index = int(len(X) * 0.8)
+    
+    X_train = X.iloc[:split_index]
+    X_test  = X.iloc[split_index:]
+    
+    y_train = y.iloc[:split_index]
+    y_test  = y.iloc[split_index:]
 
+    
     model = RandomForestRegressor(
-        n_estimators=200,
-        max_depth=12,
+        n_estimators=300,
+        max_depth=15,
         min_samples_leaf=5,
+        max_features="sqrt",
+        oob_score=True,
         n_jobs=-1,
         random_state=42
     )
 
     model.fit(X_train, y_train)
+
+    pred = model.predict(X_test)
+
     score = model.score(X_test, y_test)
     print(f"{name} model R²:", score)
+    print("OOB:", model.oob_score_)
+    
     joblib.dump(model, f"models/{name}_model.pkl")
+    
     importance = pd.Series(
         model.feature_importances_,
         index=feature_cols
@@ -71,13 +109,24 @@ def train_model(target, name):
     print(f"\nTop features for {name}:")
     print(importance.head(10))
 
+    rmse = np.sqrt(mean_squared_error(y_test, pred))
+    mae = mean_absolute_error(y_test, pred)
+    r2 = r2_score(y_test, pred)
+    
+    print(f"\n{name}")
+    print("RMSE:", rmse)
+    print("MAE:", mae)
+    print("R²:", r2)
+
+
 
 # ---------------------------
-# Train three models
+# Train models
 # ---------------------------
-train_model("PM25_target","pm25")
-train_model("O3_target","o3")
-train_model("NO2_target","no2")
+train_model("AQHI_future_1h", "aqhi_1h")
+train_model("AQHI_future_2h", "aqhi_2h")
+train_model("AQHI_future_3h", "aqhi_3h")
+train_model("AQHI_future_6h", "aqhi_6h")
 
 
 
